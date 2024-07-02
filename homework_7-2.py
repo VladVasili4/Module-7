b25c8f3b2b658a8819d145bc6779d2495e11ccc9
VladVasili4 <dipol777@ya.ru>
2024-07-02 20:31:11 +0300

    Add files via upload

--- homework_7-2.py
+++ homework_7-2.py
@@ -1,6 +1,18 @@
 import io
 from pprint import pprint
 
+def custom_write(file_name, strings):
+    file = open(file_name, 'a', encoding='UTF-8')
+    for line in strings:
+        line_ = line + '\n'
+        n = strings.index(line)
+        file.write(line_)               # Записал(добавил) строку в файл file_name
+        strings_positions ={(n, file.tell()):line}
+        print(strings_positions)
+    return strings_positions
+    print(strings_positions)
+    file.close()
+
 info = [
     'Text for tell.',
     'Используйте кодировку utf-8.',
@@ -8,19 +20,6 @@ info = [
     'Спасибо!'
     ]
 
-def custom_write(file_name, strings):
-    file = open(file_name, 'a', encoding='UTF-8')
-    print(file.writable())
-    # Как вывести или получить номер строки 'n' для формирования словаря strings_positions
-    for i in strings:
-        print(i)
-        n = len(strings)    #  ??????????
-        file.write(i)
-        print(file.tell())
-        strings_positions = {(n, file.tell()): i}
-    return strings_positions
-    file.close()
-
 result = custom_write('test.txt', info)
-# for elem in result.items():
-#     print(elem)
+
+
