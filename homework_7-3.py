f0725a9e577c30e898f20f71f617ebdd08b9753c
VladVasili4 <dipol777@ya.ru>
2024-07-03 07:24:27 +0300

    Add files via upload

--- homework_7-3.py
+++ homework_7-3.py
@@ -4,28 +4,33 @@ from pprint import pprint
 class WordsFinder:
     def __init__(self, *files):
         self.file_names = files
-    def printus(self):
         print(self.file_names)
 
     def get_all_words(self):
         self.all_words = {}
         for i in self.file_names:
-            with open(i, 'a', encoding='utf-8') as text:
-                print(text.readable())
-                print(text.writable())
-
-                # for lines in text:
-                #     print(lines)
-                #     # line = lines.lower().split()
-                #     for j in lines:
-                #         if j in [',', '.', '=', '!', '?', ';', ':', ' - ']:
-                #             del(j)
-                # print(lines, end = '')
-        # print(i)
+            with open(i, 'r', encoding='utf-8') as text:
+                for line in text:
+                    lines = line.lower()
+                    for j in lines:
+                        if j in [',', '.', '=', '!', '?', ';', ':', ' - ']:
+                            lines = lines.lower().replace(j, '')
+                    words = lines.split()
+                    self.all_words[i] = words
+                    print(self.all_words)
+        return self.all_words
 
 
-finder = WordsFinder('file1.txt, file2.txt', 'file3.txt')
-txt1 = WordsFinder('7-2-1.txt')
-txt2 = WordsFinder('homework_7-1.txt')
-txt1.printus()
+txt1 = WordsFinder('file1.txt', 'file2.txt', 'file3.txt')
 txt1.get_all_words()
+
+#     for j in lines:
+#         if j in [',', '.', '=', '!', '?', ';', ':', ' - ']:
+#             del(j)
+# print(lines, end = '')
+# print(i)                #     for j in lines:
+#                 #         if j in [',', '.', '=', '!', '?', ';', ':', ' - ']:
+#                 #             del(j)
+#                 # print(lines, end = '')
+#         # print(i)
+
