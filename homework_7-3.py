45023db92a5d6e478e279f67369a52bede815ee9
VladVasili4 <dipol777@ya.ru>
2024-07-05 07:36:21 +0300

    Add files via upload

--- homework_7-3.py
+++ homework_7-3.py
@@ -4,33 +4,40 @@ from pprint import pprint
 class WordsFinder:
     def __init__(self, *files):
         self.file_names = files
-        print(self.file_names)
 
     def get_all_words(self):
         self.all_words = {}
         for i in self.file_names:
             with open(i, 'r', encoding='utf-8') as text:
+                self.line = []
                 for line in text:
-                    lines = line.lower()
-                    for j in lines:
+                    for j in line:
                         if j in [',', '.', '=', '!', '?', ';', ':', ' - ']:
-                            lines = lines.lower().replace(j, '')
-                    words = lines.split()
-                    self.all_words[i] = words
-                    print(self.all_words)
+                            line = line.lower().replace(j, '')
+                    words = line.split()
+                    self.line = self.line + words
+            self.all_words[i] = self.line
+
         return self.all_words
 
+    def find(self, word):
+        self.word = word.lower()
+        for i in self.all_words:
+            line = self.all_words[i]
+            if self.word in line:
+                k = line.index(self.word) + 1
+                fin = {i:k}
+                print(fin)
 
-txt1 = WordsFinder('file1.txt', 'file2.txt', 'file3.txt')
-txt1.get_all_words()
 
-#     for j in lines:
-#         if j in [',', '.', '=', '!', '?', ';', ':', ' - ']:
-#             del(j)
-# print(lines, end = '')
-# print(i)                #     for j in lines:
-#                 #         if j in [',', '.', '=', '!', '?', ';', ':', ' - ']:
-#                 #             del(j)
-#                 # print(lines, end = '')
-#         # print(i)
+
+
+
+
+finder1 = WordsFinder('file1.txt', 'file2.txt', 'file3.txt')
+print(finder1.get_all_words())
+finder2 = WordsFinder('test.txt')
+print(finder2.get_all_words())
+print(finder2.find('TEXT'))
+
 
