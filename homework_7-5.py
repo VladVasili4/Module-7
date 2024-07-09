4a092cfff7b9152ac16ef19859f25224f398f56e
VladVasili4 <dipol777@ya.ru>
2024-07-09 08:11:37 +0300

    Add files via upload

--- homework_7-5.py
+++ homework_7-5.py
@@ -1,20 +1,16 @@
 import os
 import time
 
-print(os.getcwd())
-os.chdir(r'F:\Python-Urban\Homeworks\Модуль 7')
+path_ = os.getcwd()
 
 for root, dirs, files in os.walk('.'):
+  # print(dirs, files)
   for file in files:
-    filepath = os.getcwd() + file
+    filepath = os.path.join(path_, file)
     filetime = os.path.getmtime(file)
     formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
     filesize = os.path.getsize(file)
     parent_dir = os.path.dirname(file)
-
-
-    print(parent_dir)                             #  шляпа !!!!!!!!!!!
-
-
-    print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
+    print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, '
+          f'Родительская директория: {parent_dir}')
 
